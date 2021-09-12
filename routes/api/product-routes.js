f2aa8e58a2fd773fcd8fb10c91284dda6d195145
Sean <sdillon215@gmail.com>
2021-09-11 23:40:24 -0700

    get Product by id started

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -33,6 +33,28 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+    ]
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
